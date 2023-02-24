f851310431955633ac2d72c2dd4c88fed6340498
tomato-stats <stefunnie_young@yahoo.com>
2023-02-24 02:30:51 +0000

    test commit

---
+++ distancetests.R
@@ -0,0 +1,18 @@
+freq_to_prob <- function(input_hist){
+  # Input is a matrix where each row is an organism
+  max_sequence_length <- rowSums(input_hist) |> max()
+  sequence_length_deficit <- rowSums(input_hist) - max_sequence_length
+  add_these_to_all <- matrix(sequence_length_deficit / max_sequence_length, ncol= ncol(input_hist), nrow = nrow(input_hist)) 
+  input_hist <- input_hist + add_these_to_all
+  input_hist / rowSums(input_hist)
+}
+
+modify_dist <- function(){
+  
+  
+}
+
+
+
+emdist:emd2d(freq_to_prob)
+philentrophy::KL
